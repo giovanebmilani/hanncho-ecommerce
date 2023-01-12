8bc3fdfa79e0f9792eba91c7210daf72dae97ed6
giovanebmilani <giovanebmilani@hotmail.com>
2023-01-12 10:55:41 -0300

    Button and Input components

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react'
+import './index.scss'
+
+export interface ButtonProps {
+	children?: ReactNode
+	onClick?: VoidFunction
+	type?: 'primary' | 'secondary'
+	disabled?: boolean
+}
+
+export const Button: React.FC<ButtonProps> = ({
+	children,
+	onClick,
+	type = 'primary',
+	disabled = false
+}) => {
+	return (
+		<button className={`button-container ${type}`} onClick={onClick} disabled={disabled}>
+			{children}
+		</button>
+	)
+}
+
+export default Button
