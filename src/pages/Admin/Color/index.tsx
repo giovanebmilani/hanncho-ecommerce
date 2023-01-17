9c8d4145eaec1a238d2e2428c0a60a52233361a4
Giovane Bianchi Milani <giovanebmilani@hotmail.com>
2023-01-17 12:44:16 -0300

    list all colors

--- src/pages/Admin/Color/index.tsx
+++ src/pages/Admin/Color/index.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react'
 import { useGetAllColors } from '../../../api/admin/color/queries'
+import Button from '../../../components/Button'
+import ColorViewer from '../../../components/ColorViewer'
+import IconButton from '../../../components/IconButton'
 import { ColorDto } from '../../../dtos/Color'
 import './index.scss'
 
@@ -12,15 +15,49 @@ const ColorDashboard: React.FC = () => {
 		setColors(data)
 	}, [data])
 
+	const onAddClick = () => {
+		console.log('add')
+	}
+
+	const onEditClick = () => {
+		console.log('edit')
+	}
+
+	const onDeleteClick = () => {
+		console.log('delete')
+	}
+
 	return (
 		<div className='color-dashboard-container'>
 			<div className='content'>
 				<p className='title'>Cores</p>
-				{colors.map((color, index) => (
-					<p key={index}>
-						{color.name} - {color.hex}
-					</p>
-				))}
+				<div className='row-wrapper'>
+					<div className='color-list'>
+						{colors.map((color, index) => (
+							<div key={index} className='color-list-item'>
+								<div className='left-content'>
+									<ColorViewer hex={color.hex} />
+								</div>
+								<div className='middle-content'>
+									<p>{color.name}</p>
+								</div>
+								<div className='right-content'>
+									<IconButton onClick={onEditClick}>
+										<img src={process.env.PUBLIC_URL + './assets/edit-icon.png'} />
+									</IconButton>
+									<IconButton onClick={onDeleteClick}>
+										<img src={process.env.PUBLIC_URL + './assets/trash-icon.png'} />
+									</IconButton>
+								</div>
+							</div>
+						))}
+					</div>
+					<div className='buttons'>
+						<Button type='primary' onClick={onAddClick}>
+							+ ADICIONAR
+						</Button>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
