abd811dc25ebbd2145bc03a6613f6e2c1392617d
Giovane Bianchi Milani <giovanebmilani@hotmail.com>
2023-01-25 01:15:10 -0300

    initial homepage responsive

---
+++ src/components/ConcreteBeam/index.tsx
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const ConcreteBeam: React.FC = () => {
+	return (
+		<div className='concrete-beam'>
+			
+		</div>
+	)
+}
+
+export default ConcreteBeam
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,32 @@
+import ConcreteBeam from '../../components/ConcreteBeam'
+import IMAGES from '../../utils/constants/images'
 import './index.scss'
 
 const Home: React.FC = () => {
 	return (
 		<div className='home-container'>
-			<div className='sale-image-container'>
-				<img src={process.env.PUBLIC_URL + './assets/hanncho-sale.png'} />
+			<div className='images-container'>
+				<div className='sale-image-container'>
+					<img src={process.env.PUBLIC_URL + './assets/hanncho-sale.png'} />
+				</div>
+				<ConcreteBeam />
 			</div>
 
-			<div className='content'>HOME</div>
+			<div className='highlighted-products'>
+				<p className='title'>Destaques</p>
+			</div>
+
+			<div className='hanncho-info'>
+				<div className='text-container'>
+					<p className='title'>Conheça a Hanncho</p>
+					<p className='text'>
+						A Hanncho Clothing é uma marca de roupas cujo nome é inspirado na cultura urbana
+						brasileira. Visite nossa Loja de roupas para apostar em peças super estilosas e
+						exclusivos da marca.
+					</p>
+				</div>
+				{/* <img className='image' src={IMAGES.knowHannchoImage}/> */}
+			</div>
 		</div>
 	)
 }
