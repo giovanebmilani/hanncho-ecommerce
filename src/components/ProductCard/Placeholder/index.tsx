293b86e29e719c2d12dd12f0fa7ad3d1c3d5f2bf
Giovane Bianchi Milani <giovanebmilani@hotmail.com>
2023-02-13 01:07:38 -0300

    placeholder loading responsive fix

--- src/components/ProductCard/Placeholder/index.tsx
+++ src/components/ProductCard/Placeholder/index.tsx
@@ -12,8 +12,9 @@ const ProductCardPlaceholder: React.FC = () => {
 			// viewBox='0 0 400 160'
 			backgroundColor='#1b1b1b'
 			foregroundColor='#1f1f1f'
+			style={{ minWidth: 'max(16rem, 20vw)', maxWidth: 'max(16rem, 20vw)' }}
 		>
-			<rect x='5' y='330' rx='10'width='210' height='15'  />
+			<rect x='5' y='330' rx='10' width='210' height='15' />
 			<rect x='5' y='5' rx='20' width='280' height='311' />
 			<rect x='5' y='360' rx='10' width='100' height='15' />
 		</ContentLoader>
